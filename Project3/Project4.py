199752a01b3134f5ceda0771df6d62fc7a8acf63
Durgesh Nandan <durgnan45@gmail.com>
2023-08-19 12:57:39 +0530

    Project 4

--- Project3/Project4.py
+++ Project3/Project4.py
@@ -7,5 +7,8 @@ Covid = pd.read_csv('./assets/covid_19_data.csv')
 #print(Covid['State'].fillna(Covid['State'].mean(),inplace= True))
 import seaborn as sns 
 import matplotlib.pyplot as plt
-print(sns.heatmap(Covid.isnull()))
-plt.show()
+#print(sns.heatmap(Covid.isnull()))
+#plt.show()
+print(Covid.groupby('Region').sum())
+print(Covid.groupby('Region')['Confirmed'].sum())
+print(Covid.groupby('Region')['Confirmed'].sum().sort_values(ascending=False))
